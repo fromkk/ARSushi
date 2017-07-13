205702e86adafb8bb1cf19f5fb5663540fa5e129
Kazuya Ueoka <info@fromkk.info>
2017-07-13 10:38:25 +0900

    first

--- ARSushi/Objects/Sushi.swift
+++ ARSushi/Objects/Sushi.swift
@@ -7,4 +7,70 @@
 //
 
 // A7 create Sushi
+import SceneKit
+
+class Sushi: SCNNode {
+    let aspect: CGFloat = 1.0 / 100.0
+    
+    lazy var shari: SCNBox = { () -> SCNBox in
+        let shari: SCNBox = SCNBox(width: 3.0 * self.aspect, height: 2.0 * self.aspect, length: 7.0 * self.aspect, chamferRadius: 0.8 * self.aspect)
+        shari.materials = [self.rice]
+        return shari
+    }()
+    
+    lazy var rice: SCNMaterial = { () -> SCNMaterial in
+        let rice: SCNMaterial = SCNMaterial()
+        rice.diffuse.contents =  #imageLiteral(resourceName: "rice")
+        rice.lightingModel = .constant
+        return rice
+    }()
+    
+    lazy var body: SCNBox = { () -> SCNBox in
+        let box: SCNBox = SCNBox(width: 4.0 * self.aspect, height: 1.5 * self.aspect, length: 8.0 * self.aspect, chamferRadius: 0.5 * self.aspect)
+        return box
+    }()
+    
+    var material: UIImage? {
+        didSet {
+            guard let image: UIImage = self.material else { return }
+            
+            let material: SCNMaterial = SCNMaterial()
+            material.diffuse.contents = image
+            material.lightingModel = .constant
+            self.body.materials = [material]
+        }
+    }
+    
+    convenience init(material: UIImage) {
+        self.init()
+        
+        defer {
+            self.material = material
+        }
+    }
+    
+    override init() {
+        super.init()
+        
+        let shari: SCNNode = SCNNode(geometry: self.shari)
+        
+        // A12 add physicsBody to shari
+        
+        
+        self.addChildNode(shari)
+        
+        let body: SCNNode = SCNNode(geometry: self.body)
+        body.position = SCNVector3Make(0.0, 2.1 * Float(self.aspect)
+            , 0.0)
+        
+        // A13 add physicsBody to body
+        
+        
+        self.addChildNode(body)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
--- ARSushi/ViewController.swift
+++ ARSushi/ViewController.swift
@@ -21,24 +21,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // A1 sceneView setting
-        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.scene = SCNScene()
         
         // A5 add tapGesture to sceneView
-        
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.onTapGesture(_:))))
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // A2 configuration ARWorldTrackingSessionConfiguration
-        
+        if ARWorldTrackingSessionConfiguration.isSupported {
+            let configuration = ARWorldTrackingSessionConfiguration()
+            configuration.planeDetection = .horizontal
+            configuration.isLightEstimationEnabled = true
+            self.sceneView.session.run(configuration)
+        } else {
+            self.sceneView.session.run(ARSessionConfiguration())
+        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         // A3 pause ARSession
-        
+        self.sceneView.session.pause()
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,6 +58,29 @@ class ViewController: UIViewController {
 }
 
 // A4 handle tap gesture
+extension ViewController {
+    @objc private func onTapGesture(_ gesture: UITapGestureRecognizer) {
+        // A6 check hit position
+        let positions = self.sceneView.hitTest(gesture.location(in: gesture.view), types: ARHitTestResult.ResultType.estimatedHorizontalPlane) //A13 change to existingPlaneUsingExtent
+        guard let position = positions.first else { return }
+        
+        // A8 add Sushis to sceneView
+        let material: UIImage = { () -> UIImage in
+            let images: [UIImage] = [
+                #imageLiteral(resourceName: "tsuna"),
+                #imageLiteral(resourceName: "salmon")
+            ]
+            let index: Int = Int(arc4random_uniform(UInt32(images.count)))
+            return images[index]
+        }()
+        
+        let sushi: Sushi = Sushi(material: material)
+        sushi.position = SCNVector3Make(position.worldTransform.columns.3.x,
+                                        position.worldTransform.columns.3.y, //A14 add + 0.03
+            position.worldTransform.columns.3.z)
+        self.sceneView.scene.rootNode.addChildNode(sushi)
+    }
+}
 
 
 extension ViewController : ARSCNViewDelegate {
